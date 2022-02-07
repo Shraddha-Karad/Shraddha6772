d9c9e5701a3ad8715feafbd387c272c47ff75559
karad <karad@DESKTOP-L9N5TB4>
2022-02-07 15:05:20 +0530

    My first Commit with Demos.

--- Shraddha6772/src/Operators/AssignmentOperator.java
+++ Shraddha6772/src/Operators/AssignmentOperator.java
@@ -9,14 +9,13 @@ public class AssignmentOperator {
 
 		System.out.println("The Assignment Value is : ");
 
-		X+=2;// (X=X+2)
+		X+=2; // (X=X+2)
 		Y-=2; //(Y=Y-2)
 		Z*=2; //(Z=Z*2)
 		
 		System.out.println("The Value of X is : " +X); //14
 		System.out.println("The Value of Y is : " +Y); // 11
 		System.out.println("The Value of Z is : " +Z); // 32
-		
 		
 		
 	}
---
+++ Shraddha6772/src/Operators/TernaryOperator.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a=2;
+		int b=5;
+		int min=(a<b)?a:b;
+
+		System.out.println(min);
+		
+		
+	}
+
+}
