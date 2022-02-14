0313af9d13b56910d463fce625397878f25e1f2d
karad <karad@DESKTOP-L9N5TB4>
2022-02-14 13:13:13 +0530

    My First Commit with Demos.

---
+++ Shraddha6772/src/ExceptionHandling/Exception1.java
@@ -0,0 +1,20 @@
+package ExceptionHandling;
+
+public class Exception1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("ABC");
+		System.out.println("XYZ");
+		
+		int a = 100/0;
+		
+		System.out.println("Edubring");
+		System.out.println("");
+		
+		
+	}
+
+}
---
+++ Shraddha6772/src/OOPS/MethodOverriding.java
@@ -0,0 +1,39 @@
+package OOPS;
+
+class Animal{
+	
+	int z;
+	public Animal(int a){
+		System.out.println("Animal Constructor");
+	}
+	public void move(){
+		
+		System.out.println("Animal can move"+ z);
+	}
+	
+}
+class Dog extends Animal{
+	
+	public Dog(int x,int y){
+		
+		super(x);
+		super.z=50;
+		System.out.println("Dog Constructor"+x);
+	}
+	public void move(){
+
+		System.out.println("Dog can move and run");
+		
+	}
+}
+
+public class MethodOverriding {
+
+	public static void main(String[] args) {
+
+		Dog obj = new Dog(20,40);
+		obj.move();
+		
+	}
+
+}
