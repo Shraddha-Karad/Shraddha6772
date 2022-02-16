489d8091f6b2a5d77926b8f9389aa00e58188395
karad <karad@DESKTOP-L9N5TB4>
2022-02-16 20:14:59 +0530

    My First Commit

---
+++ Shraddha6772/src/Array/ArrayReturnDemo.java
@@ -0,0 +1,23 @@
+package Array;
+
+import java.util.*;
+
+public class ArrayReturnDemo {
+	
+	public static String[] return_Array() {
+		
+		// define string array
+		String[] ret_Array = { "Java", "C++", "Python", "Ruby", "C" };
+		
+		// return string array
+		return ret_Array;
+	}
+
+	public static void main(String args[]) {
+		
+		// call method return_array that returns array
+		String[] str_Array = return_Array();
+		
+		System.out.println("Array returned from method:" + Arrays.toString(str_Array));
+	}
+}
---
+++ Shraddha6772/src/Array/PassingArrayAsArgument.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class PassingArrayAsArgument {
+
+	// method to print an array, taking array as an argument
+	private static void printArray(int[] arr) // Function Definition
+	{
+		System.out.println("Array contents printed through method:");
+		// print individual elements of array using enhanced for loop
+		for (int i = 0; i < arr.length; i++) {
+
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		// integer array
+		int[] intArray = { 10, 20, 30, 40, 50, 60, 70, 80 };
+
+		// call printArray method by passing intArray as an argument
+		printArray(intArray); // Function call
+
+	}
+}
