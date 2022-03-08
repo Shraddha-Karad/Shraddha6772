107bb7e24e104fc533287b77bb2e94a1fee9a3d8
karad <karad@DESKTOP-L9N5TB4>
2022-03-08 15:34:15 +0530

    New Commit

--- Shraddha6772/src/JavaSE8/FilterDemo.java
+++ Shraddha6772/src/JavaSE8/FilterDemo.java
@@ -9,15 +9,21 @@ import java.util.stream.Stream;
 public class FilterDemo {
 
 	public static void main(String[] args) {
+		
 		List<Integer> list = new ArrayList<Integer>();
+		
 		list.add(33);
 		list.add(44);
 		list.add(55);
 		list.add(66);
 		list.add(77);
+		
 		Stream<Integer> obj = list.stream();
+		
 		// obj.forEach(e->System.out.println(e));;
+		
 		List<Integer> l2 = list.stream().filter(e -> e > 44).collect(Collectors.toList());
+		
 		System.out.println(l2);
 	}
 }
